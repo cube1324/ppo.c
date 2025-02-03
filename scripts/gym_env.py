e1448a2039d3b90925e038b4f6faaacfc6404ed6
jonas <jonas.guenster@gmx.de>
2025-02-03 16:01:19 +0100

    added python script to interface gymnasium

---
+++ scripts/gym_env.py
@@ -0,0 +1,27 @@
+import gymnasium
+
+env = None
+
+def init_env(id):
+    global env
+    env = gymnasium.make("Pendulum-v1")
+
+def reset_env():
+    obs, _ = env.reset()
+    return obs.tolist()
+
+def step_env(action):
+    obs, reward, terminated, truncated, info = env.step(action)
+    return obs.tolist(), float(reward), bool(terminated), bool(truncated)
+
+
+if __name__ == "__main__":
+    init_env(0)
+
+    obs = reset_env()
+
+    for i in range(100):
+        action = [1]
+        obs, reward, terminated, truncated = step_env(action)
+        print(obs)
+
